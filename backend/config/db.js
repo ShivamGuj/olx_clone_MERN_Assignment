8c0beaed38ab4efc14d94d3cbb8dbb2f59a606e4
ShivamGuj <shivamgujaria4@gmail.com>
2024-08-12 18:40:40 +0530

    commit MERN

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://shivamgujaria4:gQXkORC9IpYY17go@cluster0.zuc95.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      process.env.MONGO_URL,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
